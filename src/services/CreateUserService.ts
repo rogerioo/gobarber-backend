0b49c0ebf29eddf85626878dcefb5857e98f37c6
Rogério Júnior <junior.rogerio8@gmail.com>
2020-06-30 04:03:28 -0300

    Add bcrypt to encript the users password

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface Request {
@@ -19,13 +21,17 @@ class CreateUserService {
       throw new Error('Email address already used');
     }
 
+    const hashedPassword = await hash(password, 8)
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
+
+    delete user.password
 
     return user;
   }
