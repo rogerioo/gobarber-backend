de283b8e1a134f0567ae0174c2d50beb3e7f0935
Rogério Júnior <junior.rogerio8@gmail.com>
2020-06-30 02:37:54 -0300

    Remove nullability due to default behaviour of typeorm

--- src/database/migrations/1593490995004-CreateAppointments.ts
+++ src/database/migrations/1593490995004-CreateAppointments.ts
@@ -17,12 +17,10 @@ export default class CreateAppointments1593490995004
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
