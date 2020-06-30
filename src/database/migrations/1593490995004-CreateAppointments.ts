112e492f6750c8934361fccfd79abc279ab69a17
Rogério Júnior <junior.rogerio8@gmail.com>
2020-06-30 03:16:36 -0300

    Change id type to uuid

--- src/database/migrations/1593490995004-CreateAppointments.ts
+++ src/database/migrations/1593490995004-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593490995004
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
